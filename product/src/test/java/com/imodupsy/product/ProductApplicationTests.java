85b452c8ab7e0b1de811a9171a0bd1d5e49249ee
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2022-12-28 08:39:15 +0100

    add integration test for product

---
+++ product/src/test/java/com/imodupsy/product/ProductApplicationTests.java
@@ -0,0 +1,67 @@
+package com.imodupsy.product;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.imodupsy.product.dto.ProductRequestDto;
+import com.imodupsy.product.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author iModupsy
+ * @created 28/12/2022
+ */
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+public class ProductApplicationTests {
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
+        dymDynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+   @Test
+    public void shouldCreateProduct() throws Exception {
+        ProductRequestDto productRequest = getProductRequest();
+        String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productRequestString))
+                .andExpect(status().isCreated());
+        assertEquals(1, productRepository.findAll().size());
+
+    }
+
+    private ProductRequestDto getProductRequest() {
+        return ProductRequestDto.builder()
+                .name("iPhone 13")
+                .description("iPhone 13")
+                .price(BigDecimal.valueOf(1200))
+                .build();
+    }
+}
