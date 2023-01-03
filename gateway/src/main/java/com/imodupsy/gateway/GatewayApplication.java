2f5a853265959a69c39ab07a4bbd40308b69986d
Modupe Ade-Onojobi <modupeadeonojobi@gmail.com>
2023-01-03 22:26:07 +0100

    add API gateway

---
+++ gateway/src/main/java/com/imodupsy/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.imodupsy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author iModupsy
+ * @created 03/01/2023
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
